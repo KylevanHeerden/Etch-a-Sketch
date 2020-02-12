d683f2f1965d0c8255aacdfae7d0afc1465d8577
Kyle <kylevanheerden@ymail.com>
2020-02-12 10:44:12 +0200

    finished project but was with a lot of copy help

---
+++ grid.js
@@ -0,0 +1,37 @@
+function createGrid(size){
+    let grid = document.getElementById('grid');
+    let cellsize = 800 / size;
+    for (i=0; i < size; i++){
+        let row = document.createElement("div");
+        row.className = "row";
+        row.style.height = cellsize + "px";
+        for(j=0; j<size; j++){
+            let cell = createCell(cellsize);
+            row.appendChild(cell);
+        }
+        grid.appendChild(row);
+    }
+}
+
+function createCell(cellSize){
+    let cell = document.createElement("div");
+    cell.className = "cell";
+    cell.style.height = cellSize + "px";
+    cell.style.width = cellSize + "px";
+    cell.style.backgroundColor = "black";
+    cell.style.opacity = "0";
+    cell.addEventListener("mouseover", function(){
+        this.style.opacity = (this.style.opacity == 1) ? 1 : parseFloat(this.style.opacity) + 0.1;
+    })
+
+    return cell;
+
+}
+
+let reset = document.getElementById("reset");
+reset.addEventListener("click", function() {
+    document.getElementById("grid").innerHTML = "";
+    createGrid(parseInt(document.getElementById("gridSizeInput").value));
+});
+
+createGrid(parseInt(document.getElementById("gridSizeInput").value));
